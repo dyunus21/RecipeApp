674f9bc4f764fb80851763066a1c0fc819c59a2d
Diya Yunus <diyayunus@fb.com>
2022-07-25 15:25:01 -0700

    Created empty navigation drawer

--- app/src/main/java/com/example/recipeapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/recipeapp/fragments/ProfileFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -20,17 +21,13 @@ import com.bumptech.glide.Glide;
 import com.example.recipeapp.R;
 import com.example.recipeapp.activities.MainActivity;
 import com.example.recipeapp.adapters.PostsAdapter;
-import com.example.recipeapp.adapters.ProfileAdapter;
 import com.example.recipeapp.adapters.RecipeSearchAdapter;
-import com.example.recipeapp.databinding.FragmentProfileBinding;
 import com.example.recipeapp.clients.ImageClient;
-import com.example.recipeapp.models.Comment;
+import com.example.recipeapp.databinding.FragmentProfileBinding;
 import com.example.recipeapp.models.Post;
 import com.example.recipeapp.models.Recipe;
 import com.example.recipeapp.models.User;
 import com.google.android.material.tabs.TabLayout;
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -69,7 +66,7 @@ public class ProfileFragment extends Fragment {
         recipes = new ArrayList<>();
         recipeSearchAdapter = new RecipeSearchAdapter(getContext(), recipes);
         posts = new ArrayList<>();
-        // postsAdapter = new PostsAdapter(getContext(), posts);
+        postsAdapter = new PostsAdapter(getContext(), posts);
         imageClient = new ImageClient(this);
         User.getUser(CURRENT_USER);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Profile");
@@ -79,19 +76,20 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.navigationDrawerView.setVisibility(View.GONE);
         binding.tvUsername.setText("@" + CURRENT_USER.getParseUser().getUsername());
         binding.tvFullname.setText(CURRENT_USER.getFirstName() + " " + CURRENT_USER.getLastName());
         setProfileImage();
         binding.tvChangeProfileImage.setOnClickListener(v -> imageClient.onPickPhoto(v));
         binding.tvRecipeCount.setText(String.valueOf(CURRENT_USER.getRecipesUploaded().size()));
 
-        binding.tvPostCount.setText(String.valueOf(posts.size()));
 
-        binding.rvRecipes.setLayoutManager(new GridLayoutManager(getContext(),2));
+        binding.rvRecipes.setLayoutManager(new GridLayoutManager(getContext(), 2));
         binding.rvRecipes.setAdapter(recipeSearchAdapter);
 
-        binding.rvPosts.setLayoutManager(new GridLayoutManager(getContext(),2));
+        binding.rvPosts.setLayoutManager(new GridLayoutManager(getContext(), 3));
         binding.rvPosts.setAdapter(postsAdapter);
+        queryPosts();
 
         setUpTabs();
 
@@ -103,8 +101,12 @@ public class ProfileFragment extends Fragment {
             public void onTabSelected(TabLayout.Tab tab) {
                 if (tab == binding.tabLayout.getTabAt(0)) {
                     queryRecipes();
+                    binding.rvRecipes.setVisibility(View.VISIBLE);
+                    binding.rvPosts.setVisibility(View.GONE);
                 } else if (tab == binding.tabLayout.getTabAt(1)) {
-                    queryPosts();
+                    binding.rvRecipes.setVisibility(View.GONE);
+                    binding.rvPosts.setVisibility(View.VISIBLE);
+
                 }
             }
 
@@ -117,30 +119,34 @@ public class ProfileFragment extends Fragment {
             public void onTabReselected(TabLayout.Tab tab) {
                 if (tab == binding.tabLayout.getTabAt(0)) {
                     queryRecipes();
+                    binding.rvRecipes.setVisibility(View.VISIBLE);
+                    binding.rvPosts.setVisibility(View.GONE);
                 } else if (tab == binding.tabLayout.getTabAt(1)) {
-                    queryPosts();
+                    binding.rvRecipes.setVisibility(View.GONE);
+                    binding.rvPosts.setVisibility(View.VISIBLE);
+
                 }
             }
         });
     }
 
     private void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery("Post");
-        query.whereEqualTo(Post.KEY_AUTHOR,CURRENT_USER);
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.whereEqualTo(Post.KEY_AUTHOR, CURRENT_USER.getParseUser());
         query.include(Post.KEY_TITLE);
         query.include(Post.KEY_AUTHOR);
         query.include(Post.KEY_IMAGE);
         query.orderByDescending(Post.KEY_CREATED_AT);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if (e!=null) {
-                    Log.e(TAG, "Unable to fetch posts",e);
-                    return;
-                }
-                posts = objects;
-                postsAdapter.addAll(posts);
+        query.findInBackground((objects, e) -> {
+            if (e != null) {
+                Log.e(TAG, "Unable to fetch posts", e);
+                return;
             }
+            postsAdapter.clear();
+            posts = objects;
+            Log.i(TAG, "Successfully fetched posts" + posts.toString());
+            postsAdapter.addAll(posts);
+            binding.tvPostCount.setText(String.valueOf(posts.size()));
         });
 
     }
@@ -188,6 +194,18 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onPrepareOptionsMenu(@NonNull Menu menu) {
         menu.findItem(R.id.logout).setVisible(true);
+        menu.findItem(R.id.navigation_drawer).setVisible(true);
         super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.navigation_drawer) {
+            if (binding.navigationDrawerView.getVisibility() == View.VISIBLE)
+                binding.navigationDrawerView.setVisibility(View.GONE);
+            else
+                binding.navigationDrawerView.setVisibility(View.VISIBLE);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
